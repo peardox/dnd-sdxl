c4199cf576267098007806b046ff8161a9784454
peardox <simon@peardox.com>
2024-01-25 20:04:36 +0000

    Tidy up several Setups

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ image_width = 512
 image_type = "jpg"
 # use_cuda is mainly for servers with NVIDIA cards. Probably irrelevant for Windows 10/11 but included 'just in case'
 # If you get an out of memory error with this set to True just change it to False - this happens if your GPU don't have enough RAM
-use_cuda = True
+use_cuda = False
 # big_mac enables Mac-ARM to use more memory. If you've got an e.g. an M1 with 8G RAM or an x86 Mac then this should be false
 big_mac = False
 
